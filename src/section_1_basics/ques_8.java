e80ce026f4a85563d969b731934280fd8ddcf9a6
NirbhayVashisht <nirbhayvashisht@gmail.com>
2020-03-21 16:56:44 +0530

    ques_8 added

--- src/section_1_basics/ques_8.java
+++ src/section_1_basics/ques_8.java
@@ -1,27 +1,26 @@
-/*
-*
-**
-***
+/* pattern
+ ***
+ **
+ *
  */
 package section_1_basics;
 
 import java.util.Scanner;
 
-public class ques_7 {
+public class ques_8 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
         int n = obj.nextInt();
-
-        int nst,ist;
-        nst = 1;
-        while (nst<=n){
-            ist =1 ;
-            while (ist<=nst){
+        int nst, cst;
+        nst = n;
+        while (nst >= 1) {
+            cst = 1;
+            while (cst<=nst){
                 System.out.print("* ");
-                ist++;
+                cst++;
             }
             System.out.println();
-            nst++;
+            nst--;
         }
     }
 }
