b03c6bf019ea5247c8db7fe288cdc0e7350d0bb4
NirbhayVashisht <nirbhayvashisht@gmail.com>
2020-03-22 22:04:04 +0530

    ques_14 added

---
+++ src/section_1_basics/ques_14.java
@@ -0,0 +1,37 @@
+/*
+  1
+ 111
+11111
+ */
+package section_1_basics;
+
+import java.util.Scanner;
+
+public class ques_14 {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int nst,nsp,cst,csp;
+        nst = 1;
+        nsp = n-1;
+
+        while (n>=1){
+            csp=1;
+            while (csp<=nsp){
+                System.out.print(" ");
+                csp++;
+            }
+
+            cst=1;
+            while (cst<=nst){
+                System.out.print("1");
+                cst++;
+            }
+
+            System.out.println();
+            nsp--;
+            nst+=2;
+            n--;
+        }
+    }
+}
